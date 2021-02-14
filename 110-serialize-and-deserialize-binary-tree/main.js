d26e688fb24ad8f688a0f7a4d3107b5b3b9ac685
cola119 <k.cola119@ezweb.ne.jp>
2021-02-14 23:44:46 +0900

    serialize

--- 110-serialize-and-deserialize-binary-tree/main.js
+++ 110-serialize-and-deserialize-binary-tree/main.js
@@ -44,16 +44,38 @@ function test_serialize() {
  * @return {Node}
  */
 function deserialize(str) {
-  // your code here
+  if (str === "[]") return null;
+  const values = str
+    .match(/\[(.*)\]/)[1]
+    .split(",")
+    .map((s) => (s === "null" ? null : parseInt(s, 0)));
+  const root = new Node(values.shift());
+  const queue = [root];
+  while (values.length > 0) {
+    const node = queue.shift();
+    if (!node) continue;
+    const left = values.shift();
+    const leftNode = left ? new Node(left) : null;
+    node.left = leftNode;
+    const right = values.shift();
+    const rightNode = right ? new Node(right) : null;
+    node.right = rightNode;
+    queue.push(leftNode);
+    queue.push(rightNode);
+  }
+  return root;
 }
 function test_deserialize() {
-  console.log(deserialize("[1]"));
-  console.log(deserialize("[1,2]"));
-  console.log(deserialize("[1,2,3]"));
-  console.log(deserialize("[1,2,3,4]"));
-  console.log(deserialize("[1,2,3,4,null,null,5]"));
-  console.log(deserialize("[1,2,3,4,null,null,5,6,7,8,null,null,null,null,9]"));
+  console.log(serialize(deserialize("[]")));
+  console.log(serialize(deserialize("[1]")));
+  console.log(serialize(deserialize("[1,2]")));
+  console.log(serialize(deserialize("[1,2,3]")));
+  console.log(serialize(deserialize("[1,2,3,4]")));
+  console.log(serialize(deserialize("[1,2,3,4,null,null,5]")));
+  console.log(
+    serialize(deserialize("[1,2,3,4,null,null,5,6,7,8,null,null,null,null,9]"))
+  );
 }
 
 test_serialize();
-// test_deserialize();
+test_deserialize();
