e973fdea4e66d9e5e3887008d631efcc198c8b09
cola119 <k.cola119@ezweb.ne.jp>
2021-02-14 22:41:18 +0900

    serialize

--- 110-serialize-and-deserialize-binary-tree/main.js
+++ 110-serialize-and-deserialize-binary-tree/main.js
@@ -2,9 +2,6 @@
 // you can use this directly as it is bundled with your code
 
 class Node {
-  value;
-  left;
-  right;
   constructor(val, left = null, right = null) {
     this.value = val;
     this.left = left;
@@ -17,9 +14,22 @@ class Node {
  * @return {string}
  */
 function serialize(root) {
-  // your code here
+  if (!root) return "[]";
+  const values = [];
+  const queue = [];
+  queue.push(root);
+  while (queue.length > 0) {
+    const node = queue.shift();
+    values.push(node ? node.value : null);
+    if (node && (node.right || node.left)) {
+      queue.push(node.left);
+      queue.push(node.right);
+    }
+  }
+  return `[${values.reduce((p, v) => (p === "" ? v : `${p},${v}`), "")}]`;
 }
 function test_serialize() {
+  console.log(serialize()); // []
   console.log(serialize(new Node(1))); // [1]
   console.log(serialize(new Node(1, new Node(2), new Node(3)))); // [1,2,3]
   console.log(
@@ -44,3 +54,6 @@ function test_deserialize() {
   console.log(deserialize("[1,2,3,4,null,null,5]"));
   console.log(deserialize("[1,2,3,4,null,null,5,6,7,8,null,null,null,null,9]"));
 }
+
+test_serialize();
+// test_deserialize();
